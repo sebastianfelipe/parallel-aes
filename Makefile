9d7330d064a4623d7fcec5b16e7ccfc1c699be2c
Sebastián Felipe Torres Garfe <sebastian.torres.garfe@gmail.com>
2017-06-26 03:07:28 -0400

    update

--- Makefile
+++ Makefile
@@ -10,13 +10,13 @@ all: prsa
 # main.o:
 #
 prsa:  main.o
-	$(CC) $(CFLAGS) -o prsa main.o
+	$(CC) $(CFLAGS) -o prsa main.o -lssl -lcrypto
 
 # To create the object file main.o, we need the source
 # files main.cxx:
 #
 main.o:  main.cxx
-	$(CC) $(CFLAGS) -c main.cxx
+	$(CC) $(CFLAGS) -c main.cxx -I /usr/local/opt/openssl/include
 
 # To start over from scratch, type 'make clean'.  This
 # removes the executable file, as well as old .o object
--- main.cxx
+++ main.cxx
@@ -6,6 +6,8 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <openssl/aes.h>
+
 // #include <ctime>
 
 // DEBUG
@@ -17,6 +19,9 @@ std::vector<std::string> FILE_PATHS;
 
 // ENCRYPT
 bool ENCRYPT = true;
+
+// BASE_PATH
+std::string BASE_PATH = "./tests/bunch-1";
 
 // PASSWORD
 std::string PASSWORD = "passpass";
@@ -30,6 +35,63 @@ unsigned TOTAL_OPERATIONS;
 // STEP
 unsigned STEP;
 
+const char ivecstr[AES_BLOCK_SIZE] = "MASTER_KEY\0";
+
+void *encrypt_file(std::string input, std::string output, std::string password) {
+	int bytes_read, bytes_written;
+  	unsigned char indata[AES_BLOCK_SIZE];
+  	unsigned char outdata[AES_BLOCK_SIZE];
+
+  	/* ckey and ivec are the two 128-bits keys necesary to
+     en- and recrypt your data.  Note that ckey can be
+     192 or 256 bits as well */
+  	unsigned char *ckey = (unsigned char *) password.c_str();
+  	// unsigned char *ivec = (unsigned char *) password.c_str();
+
+	unsigned char ivec[AES_BLOCK_SIZE];
+	memcpy( ivec , ivecstr, AES_BLOCK_SIZE);
+
+  	/* data structure that contains the key itself */
+	  AES_KEY key;
+
+	  /* set the encryption key */
+	  AES_set_encrypt_key(ckey, 256, &key);
+
+	  /* set where on the 128 bit encrypted block to begin encryption*/
+	  int num = 0;
+	  FILE *ifp = fopen(input.c_str(), "rb");
+	  FILE *ofp = fopen(output.c_str(), "wb");
+
+	  while (1) {
+	    bytes_read = fread(indata, 1, AES_BLOCK_SIZE, ifp);
+	    if (ENCRYPT)
+	    {
+	    	AES_cfb128_encrypt(indata, outdata, bytes_read, &key, ivec, &num,
+	           AES_ENCRYPT);
+	    }
+	    else
+	    {
+	    	AES_cfb128_encrypt(indata, outdata, bytes_read, &key, ivec, &num,
+	           AES_DECRYPT);
+	    }
+	    bytes_written = fwrite(outdata, 1, bytes_read, ofp);
+	    if (bytes_read < AES_BLOCK_SIZE)
+	  		break;
+	  }
+
+	  fclose(ifp);
+	  fclose(ofp);
+
+
+	// AES_KEY enc_key, dec_key;
+
+	// AES_set_encrypt_key((const unsigned char *) password.c_str(), 128, &enc_key);
+
+	// AES_set_decrypt_key((const unsigned char *) password.c_str(), 128, &dec_key);
+
+	return NULL;
+}
+
 void *encrypt_step(std::string path) {
 	std::string command;
 	std::string tmp;
@@ -41,12 +103,16 @@ void *encrypt_step(std::string path) {
 	command += " -out";
 	command += " " + tmp;
 	command += " -k " + PASSWORD;
+	
 
-	system(command.c_str());
+	// system(command.c_str());
+	encrypt_file(path, tmp, PASSWORD);
 
 	// Replace the original file by the temporal file
 	command = "mv " + tmp + " " + path;
 	system(command.c_str());
+
+	return NULL;
 }
 
 void *decrypt_step(std::string path) {
@@ -61,11 +127,14 @@ void *decrypt_step(std::string path) {
 	command += " " + tmp;
 	command += " -k " + PASSWORD;
 
-	system(command.c_str());
+	// system(command.c_str());
+	encrypt_file(path, tmp, PASSWORD);
 
 	// Replace the original file by the temporal file
 	command = "mv " + tmp + " " + path;
 	system(command.c_str());
+
+	return NULL;
 }
 
 void *rsa(void *start) {
@@ -85,25 +154,31 @@ void *rsa(void *start) {
 
 	if (ENCRYPT)
 	{
+		if (DEBUG) printf("This thread encrypt from file %d to file %d\n", first + 1 , last + 1);
 		for (int n = first; n <= last; n++)
 		{
 			encrypt_step(FILE_PATHS.at(n));
+			if (DEBUG) printf("File %d encrypted\n", n + 1);
 		}
 	}
 	else
 	{
+		if (DEBUG) printf("This thread decrypt from file %d to file %d\n", first + 1, last + 1);
 		for (int n = first; n <= last; n++)
 		{
 			decrypt_step(FILE_PATHS.at(n));
+			if (DEBUG) printf("File %d decrypted\n", n + 1);
 		}
 	}
+
+	return NULL;
 
 	// Remove the original file
 
 	// Erase the .tmp extension of the new file
 }
 
-void *find(std::string base_path) {
+void find(std::string base_path) {
 	std::string command;
 	std::string tmp("files.tmp");
 
@@ -145,49 +220,54 @@ int main(int argc, char *argv[])
 
 	// std::time_t initialTime, finishTime;
 
-	// // Configuring Parameters
-	// for (int i = 1; i < argc; i++)
-	// {
-	// 	if (argv[i] == std::string("-n"))
-	// 	{
-	// 		N = atoi(argv[i+1]);
-	// 	}
+	// Configuring Parameters
+	for (int i = 1; i < argc; i++)
+	{
 
-	// 	else if (argv[i] == std::string("-m"))
-	// 	{
-	// 		M = atoi(argv[i+1]);
-	// 	}
+		if (argv[i] == std::string("-password") || argv[i] == std::string("-k"))
+		{
+			PASSWORD = argv[i+1];
+		}
 
-	// 	else if (argv[i] == std::string("-p"))
-	// 	{
-	// 		P = atoi(argv[i+1]);
-	// 	}
+		else if (argv[i] == std::string("-path") || argv[i] == std::string("-p"))
+		{
+			BASE_PATH = argv[i+1];
+		}
 
-	// 	else if (argv[i] == std::string("-t"))
-	// 	{
-	// 		PARALLEL = true;
-	// 		THREADS = atoi(argv[i+1]);
-	// 	}
+		else if (argv[i] == std::string("-threads") || argv[i] == std::string("-t"))
+		{
+			PARALLEL = true;
+			THREADS = atoi(argv[i+1]);
+		}
 
-	// 	else if (argv[i] == std::string("--parallel") || argv[i] == std::string("--p"))
-	// 	{
-	// 		PARALLEL = true;
-	// 	}
+		else if (argv[i] == std::string("--parallel") || argv[i] == std::string("--p"))
+		{
+			PARALLEL = true;
+		}
 
-	// 	else if (argv[i] == std::string("--verbose") || argv[i] == std::string("--v"))
-	// 	{
-	// 		DEBUG = true;
-	// 	}
-	// }
+		else if (argv[i] == std::string("--verbose") || argv[i] == std::string("--v"))
+		{
+			DEBUG = true;
+		}
 
+		else if (argv[i] == std::string("--encrypt") || argv[i] == std::string("--e"))
+		{
+			ENCRYPT = true;
+		}
+
+		else if (argv[i] == std::string("--decrypt") || argv[i] == std::string("--d"))
+		{
+			ENCRYPT = false;
+		}
+	}
 
 
 	// Generate random vectors values (matrix representation)
 	// Initialize vector A
 
-	std::cout << "Setting file paths..." << std::endl;
+	if (DEBUG) std::cout << "Setting file paths..." << std::endl;
 
-	find("/Users/tiantg/Projects/parallel-rsa/tests/lorem-ipsum");
+	find(BASE_PATH);
 
 	TOTAL_OPERATIONS = FILE_PATHS.size();
 	THREADS = (unsigned) fmin(THREADS, TOTAL_OPERATIONS);
@@ -198,20 +278,28 @@ int main(int argc, char *argv[])
 		std::cout << "ERROR: No files to encrypt/decrypt";
 		return 1;
 	}
-	else if (TOTAL_OPERATIONS == 1)
-	{
-		std::cout << TOTAL_OPERATIONS << " file has been set" << std::endl;
-	}
-	else
-	{
-		std::cout << TOTAL_OPERATIONS << " files has been set" << std::endl;
-	}
 
-	std::cout << std::endl;
+	if (DEBUG)
+	{
+		if (TOTAL_OPERATIONS == 1)
+		{
+			std::cout << TOTAL_OPERATIONS << " file has been set" << std::endl;
+		}
+		else
+		{
+			std::cout << TOTAL_OPERATIONS << " files has been set" << std::endl;
+		}
+		std::cout << "BASE_PATH: " << BASE_PATH << std::endl;
+		std::cout << "PASSWORD: " << PASSWORD << std::endl;
+		std::cout << "PARALLEL: " << (PARALLEL ? "true" : "false") << std::endl;
+		std::cout << "THREADS: " << THREADS << std::endl;
+		
+
+	}
 
 	// Threads init
 
-	printf("Start encrypting...\n");
+	if (DEBUG) std::cout << "Start " << (ENCRYPT ? "encrypting" : "decrypting") << " process..." << std::endl;
 
 	// encrypt_step("/Users/tiantg/Projects/parallel-rsa/tests/lorem-ipsum/data.txt");
 	// decrypt_step("/Users/tiantg/Projects/parallel-rsa/tests/lorem-ipsum/data.txt");
@@ -221,11 +309,12 @@ int main(int argc, char *argv[])
 	// Parallel Version
 	if (PARALLEL)
 	{
-		std::cout << "Creating Threads..." << std::endl;
+		if (DEBUG) std::cout << "Creating Threads..." << std::endl;
+		
 		threads_tab = (pthread_t *)malloc( THREADS*sizeof(pthread_t));
 		thread_args = (unsigned *)malloc(sizeof(unsigned)*THREADS);
 		// if (DEBUG) std::cout << "Threads Initialized" << std::endl;
-		std::cout << "Threads Initialized" << std::endl;
+		if (DEBUG) std::cout << "Threads Initialized" << std::endl;
 		for (unsigned i = 0; i < THREADS; i++)
 		{
 			thread_args[i] = i*STEP;
@@ -268,7 +357,7 @@ int main(int argc, char *argv[])
 	// 	std::cout << std::endl << std::endl;
 	// }
 
-	std::cout << "Finish encrypting" << std::endl;
+	if (DEBUG) std::cout << "Finish " << (ENCRYPT ? "encrypting" : "decrypting") << " process successfully"<< std::endl;
 	// std::cout <<  "Timing: " << timeElapsed << "[s]" << std::endl;
 
 	return 0;
